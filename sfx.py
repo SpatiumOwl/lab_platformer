86a67bb71d1d22b2356331bd79815246d2d1726e
SpatiumOwl <kiaz21.fe@gmail.com>
2022-10-24 16:35:38 +0300

    SpatiumOwl. Added Global volume

--- sfx.py
+++ sfx.py
@@ -3,21 +3,22 @@ import os
 
 class SFX:
     pygame.mixer.init()
+    GLOBAL_VOLUME = 0.5
 
     BGM = pygame.mixer.Sound(os.path.join("Sound", "BGM_Mission_Stage_Crater_01_A [1].mp3"))
-    BGM.set_volume(0.3)
+    BGM.set_volume(0.3 * GLOBAL_VOLUME)
 
     GOT_COIN = pygame.mixer.Sound(os.path.join("Sound", "VoiceFDefeat01.wav"))
-    GOT_COIN.set_volume(0.5)
+    GOT_COIN.set_volume(0.5 * GLOBAL_VOLUME)
 
     NEXT_LEVEL = pygame.mixer.Sound(os.path.join("Sound", "VoiceFJet00.wav"))
-    NEXT_LEVEL.set_volume(0.5)
+    NEXT_LEVEL.set_volume(0.5 * GLOBAL_VOLUME)
 
     DEATH = pygame.mixer.Sound(os.path.join("Sound", "VoiceFSoul03.wav"))
-    DEATH.set_volume(0.4)
+    DEATH.set_volume(0.4 * GLOBAL_VOLUME)
 
     CLOSED_GAME = pygame.mixer.Sound(os.path.join("Sound", "MusicRIP.mp3"))
-    CLOSED_GAME.set_volume(0.5)
+    CLOSED_GAME.set_volume(0.5 * GLOBAL_VOLUME)
 
     FINISHED_GAME = pygame.mixer.Sound(os.path.join("Sound", "MusicOnward.mp3"))
-    FINISHED_GAME.set_volume(0.5)
+    FINISHED_GAME.set_volume(0.5 * GLOBAL_VOLUME)
